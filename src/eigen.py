d325ebbc71f32aad7f691f6782b68eb7f92266da
Mifkiyan <m.favian199@gmail.com>
2022-11-11 05:03:58 +0700

    update eigen

--- src/eigen.py
+++ src/eigen.py
@@ -1,4 +1,5 @@
 import numpy as np
+import extract
 
 def mean(arr):
     result = [0] * 2048
@@ -13,6 +14,34 @@ def selisih(arr, mean):
     return arr
 
 def kovarian(arr):
-    trans_arr = np.transpose(arr)
-    covarian = np.matmul(arr, trans_arr)
+    compute = selisih(arr, mean(arr))
+    trans = np.transpose(compute)
+    covarian = np.matmul(compute, trans)
     return covarian
+
+def eigen_qr(arr, iterations=10000):
+    Ak = np.copy(arr)
+    n = Ak.shape[0]
+    QQ = np.eye(n)
+    for k in range(iterations):
+        s = Ak.item(n-1, n-1)
+        smult = s * np.eye(n)
+        Q, R = np.linalg.qr(np.subtract(Ak, smult))
+        Ak = np.add(R @ Q, smult)
+        QQ = QQ @ Q
+    return Ak, QQ
+
+
+# Testing
+"""
+path = 'src\dataset\pins_Adriana Lima'
+
+vec = extract.batch_extractor(path)
+kov = kovarian(vec)
+print(np.shape(kov))
+eigval, eigvec = np.linalg.eig(kov)
+eigqr, eigvecqr = eigen_qr(kov)
+
+print(eigvecqr, "\n\n")
+print(eigvec)
+"""
