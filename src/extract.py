0e5bfadc881ed2ad66ab8e21aa6ddbdb6860b560
Febryan Arota <13521120@mahasiswa.itb.ac.id>
2022-11-10 20:00:04 +0700

    add eigen

--- src/extract.py
+++ src/extract.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from matplotlib.pyplot import imread
 import sys
-np.set_printoptions(threshold=sys.maxsize)
+from numpy.linalg import eig
 
 # Feature extractor
 def extract_features(image_path, vector_size=32):
@@ -45,30 +45,3 @@ def batch_extractor(images_path):
         i += 1 
     result = list(result.values())
     return result
-
-def mean(arr):
-    result = [0] * 2048
-    for i in range(len(arr)):
-        result = np.add(result, arr[i])
-    result = result / len(arr)
-    return result
-
-def selisih(arr, mean):
-    for i in range(len(arr)):
-        arr[i] = arr[i] - mean
-    return arr
-
-# path0 = 'src\dataset\pins_Adriana Lima\Adriana Lima0_0.jpg'
-# path1 = 'src\dataset\pins_Adriana Lima\Adriana Lima1_1.jpg'
-# path = 'src\dataset\pins_Adriana Lima'
-
-# res = batch_extractor(path)
-# mean_res = mean(res)
-
-# A= np.array(selisih(res, mean_res))
-# AT= A.transpose()
-# print("yeah")
-# covarian = np.matmul(A, AT)
-# print(covarian.shape)
-# print(covarian)
-
